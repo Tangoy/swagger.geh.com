6f48d67f147341605a68ffaef68078717e6ef572
gehui <echo.ge@yingzt.com>
2018-12-05 10:08:03 +0800

    init

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,39 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Test")
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting1")
+    public Greeting greeting1(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @PostMapping("/greeting2")
+    public Greeting greeting2(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping(value = "/greeting3", method = RequestMethod.GET)
+    public Greeting greeting3(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping(value = "/greeting4", method = RequestMethod.POST)
+    public Greeting greeting4(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
